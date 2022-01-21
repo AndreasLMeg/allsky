2c6f18beb9b9b4d695ee1ffd82e7ca84e6d8f76c
AndreasLMeg <85451655+AndreasLMeg@users.noreply.github.com>
2022-01-21 17:59:20 +0100

    test: some mocks to test saveImage.sh WIP

---
+++ test/integration/mock_config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# mocking config.sh
+# usage:
+#    mock_config mock    ....... enables mocking
+#    mock_config         ....... disables mocking
+#
+#   /home/pi/allsky/config/config.sh
+
+BASEDIR=$(dirname "$0")
+source ${BASEDIR}/mock_variables
+
+if [ "$1" == "mock" ]; then
+	if [ ! -e "${ALLSKY_HOME}/config/config.sh_original" ] ; then
+		cp ${ALLSKY_HOME}/config/config.sh ${ALLSKY_HOME}/config/config.sh_original 
+	fi
+	cp "${BASEDIR}/config_mocked" ${ALLSKY_HOME}/config/config.sh
+
+	echo "${ALLSKY_HOME}config/config.sh mocked"
+else
+	cp ${ALLSKY_HOME}/config/config.sh_original ${ALLSKY_HOME}/config/config.sh
+	echo "${ALLSKY_HOME}/config/config.sh democked"
+fi
+
