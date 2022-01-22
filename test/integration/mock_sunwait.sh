6326cb5965402298060f21631a2113bd54a25b42
AndreasLMeg <85451655+AndreasLMeg@users.noreply.github.com>
2022-01-22 06:18:18 +0100

    test: use /usr/bin/sunwait

--- test/integration/mock_sunwait.sh
+++ test/integration/mock_sunwait.sh
@@ -10,14 +10,14 @@ BASEDIR=$(dirname "$0")
 source ${BASEDIR}/mock_variables
 
 if [ "$1" == "mock" ]; then
-	if [ ! -e "/usr/local/bin/sunwait_original" ] ; then
-		sudo cp /usr/local/bin/sunwait /usr/local/bin/sunwait_original
+	if [ ! -e "/usr/bin/sunwait_original" ] ; then
+		sudo cp /usr/bin/sunwait /usr/bin/sunwait_original
 	fi
-	sudo cp "${BASEDIR}/sunwait_mocked" /usr/local/bin/sunwait
+	sudo cp "${BASEDIR}/sunwait_mocked" /usr/bin/sunwait
 
 	echo "sunwait mocked"
 else
-	sudo cp /usr/local/bin/sunwait_original /usr/local/bin/sunwait
+	sudo cp /usr/bin/sunwait_original /usr/bin/sunwait
 	echo "sunwait democked"
 fi
 
