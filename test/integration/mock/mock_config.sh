c26c38832583514080c9d6f94884d2a375f0272c
AndreasLMeg <85451655+AndreasLMeg@users.noreply.github.com>
2022-02-14 19:08:41 +0100

    test: make config.sh

--- test/integration/mock/mock_config.sh
+++ test/integration/mock/mock_config.sh
@@ -11,10 +11,14 @@ BASEDIR=$(dirname "$0")
 source ${BASEDIR}/mock_variables
 
 if [ "$1" == "mock" ]; then
+	if [ ! -d "${ALLSKY_HOME}/config" ] ; then
+		mkdir -p "${ALLSKY_HOME}/config" 
+		cp "${ALLSKY_HOME}/config_repo/config.sh.repo" ${ALLSKY_HOME}/config/config.sh
+	fi
+
 	if [ ! -e "${ALLSKY_HOME}/config/config.sh_original" ] ; then
 		cp ${ALLSKY_HOME}/config/config.sh ${ALLSKY_HOME}/config/config.sh_original 
 	fi
-	#cp "${BASEDIR}/config_mocked" ${ALLSKY_HOME}/config/config.sh
 
 	echo "${ALLSKY_HOME}config/config.sh mocked"
 else
