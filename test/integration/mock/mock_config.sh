6cb8317b34a6c825a4163e84d84e511abdaebdc2
AndreasLMeg <85451655+AndreasLMeg@users.noreply.github.com>
2022-02-14 19:27:15 +0100

    test: debug output

--- test/integration/mock/mock_config.sh
+++ test/integration/mock/mock_config.sh
@@ -11,12 +11,15 @@ BASEDIR=$(dirname "$0")
 source ${BASEDIR}/mock_variables
 
 if [ "$1" == "mock" ]; then
-	if [ ! -d "${ALLSKY_HOME}/config" ] ; then
+  ls -la "${ALLSKY_HOME}"
+	if [ ! -d "${ALLSKY_HOME}/config" ]; then
 		mkdir -p "${ALLSKY_HOME}/config" 
 		cp "${ALLSKY_HOME}/config_repo/config.sh.repo" ${ALLSKY_HOME}/config/config.sh
+	else
+	  echo "${ALLSKY_HOME}/config exists"
 	fi
 
-	if [ ! -e "${ALLSKY_HOME}/config/config.sh_original" ] ; then
+	if [ ! -e "${ALLSKY_HOME}/config/config.sh_original" ]; then
 		cp ${ALLSKY_HOME}/config/config.sh ${ALLSKY_HOME}/config/config.sh_original 
 	fi
 
